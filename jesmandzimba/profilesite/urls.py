a2293293930feb385ae6d2118b67cc8a465b4ec3
jesmandzimba <jesman@baselinesoftware.co.za>
2020-02-28 14:26:05 +0200

    inital commit of prifile site

---
+++ jesmandzimba/profilesite/urls.py
@@ -0,0 +1,10 @@
+"""jesmandzimba URL Configuration
+
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('resume', views.resume, name="resume")
+]
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\symu\jesmandzimba\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
